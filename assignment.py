3320d48b0f6de8b9c8a07d807aefd46fb533aa63
Soyombo <soyombo.b2015@orkhonschool.edu.mn>
2026-09-11 15:21:16 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,6 +1,12 @@
-# Exercise 1
 def fibonacci(n):
-    pass
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+
+
 
 # Exercise 2
 def count_digits(n):
